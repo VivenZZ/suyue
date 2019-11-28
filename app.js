cae2af692d268899f1ea67ad004cb780a832e89c
VivenZ <17706500512@163.com>
2019-11-28 22:38:38 +0800

    请求书籍接口

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.all('/api/*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 // 添加路由
 router(app);
 
